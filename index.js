76ee8a3386953cadfebd1e061ae301c449b9bcc4
KirkWest <126360978+KirkWest@users.noreply.github.com>
2023-10-17 20:36:44 +1100

    controllers started, crud following

--- index.js
+++ index.js
@@ -1,21 +1,32 @@
-// imort the server package
+// import the server package 
 const bananas = require('express');
-
-// make as instance of the server that we can customise and run
+// make an instance of the server that we can customise and run 
 const app = bananas();
 
-// GET localhost:3000/
-// app.get(route path, callback funciton)
+const HOST = process.env.HOST || 'localhost';
+const PORT = process.env.PORT || 3000;
+// Ports can only be a integer between 1001 - 65536 
+
+
+// some comment here to trigger nodemon into restarting
+
+// GET localhost:3000/ 
+// app.get(route path, callback function)
 app.get("/", (request, response) => {
 
-  response.send("Hellow world, this server is bananas!");
+	response.send("Hello world, this server is bananas!");
 
 });
 
-// Configure the server -- happens all above this line ^^^^^^^
+const someImportedRouterYay = require('./controllers/PokemonController');
+app.use("/pokemon", someImportedRouterYay);
 
-//  Activate the server -- happens all below this line vvvvvv
 
-app.listen(3000, () => {
-  console.log("Server is running on port: " + 3000);
+
+// Configure the server -- happens all above this line ^^^^^^
+
+// Activate the server -- happens all below this line vvvvvvv
+
+app.listen(PORT, HOST, () => {
+	console.log("Server is running on port: " + PORT);
 });
